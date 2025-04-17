c5b1226450476cd2bc8e0f475913ce445f471b3b
PedroHCarrijo <142514100+Pedro-Henrique-Carrijo@users.noreply.github.com>
2025-04-17 14:45:47 -0300

    13.py

--- 13.py
+++ 13.py
@@ -4,6 +4,6 @@ def fatorial(numero):
         resultado *= i
     return resultado
 
-numero = int(input("Digite um número para calcular o fatorial: "))
+numero = int(input("Me fale um numero para calcular o fatorial: "))
 resultado = fatorial(numero)
 print(f"O fatorial de {numero} é {resultado}.")
