d1b18fc873a2b996bf38093ede0020dc8c97e0db
PedroHCarrijo <142514100+Pedro-Henrique-Carrijo@users.noreply.github.com>
2025-04-17 14:46:28 -0300

    14.py

--- 14.py
+++ 14.py
@@ -1,9 +1,9 @@
-def divisiveis_por_7():
+def divisiveispor7():
     contador = 0
     for i in range(1, 1001):
         if i % 7 == 0:
             contador += 1
     return contador
 
-resultado = divisiveis_por_7()
+resultado = divisiveispor7()
 print("Quantidade de números entre 1 e 1000 divisíveis por 7:", resultado)
